8aa44125dba71f59d360a21a0cf2715b04142f9e
yukimura1227 <takamura1227@gmail.com>
2017-02-03 12:11:10 +0900

    create .ideavimrc

--- create_link_to_dotfiles.sh
+++ create_link_to_dotfiles.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # careate symbolic link
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
 ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
