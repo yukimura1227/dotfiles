ad269f21fc530140e2f7895765529f52b7517879
yukimura1227 <takamura1227@gmail.com>
2017-02-25 11:37:30 +0900

    fix

--- create_link_to_dotfiles.sh
+++ create_link_to_dotfiles.sh
@@ -4,4 +4,5 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.tigrc ~/.tigrc
