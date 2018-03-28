22e9efc43ed6f1301708bef91d966187e90762b1
yukimura1227 <takamura1227@gmail.com>
2018-03-28 11:26:09 +0900

    add gitignore_global

--- create_link_to_dotfiles.sh
+++ create_link_to_dotfiles.sh
@@ -5,6 +5,7 @@ ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
 ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
 ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.tigrc ~/.tigrc
