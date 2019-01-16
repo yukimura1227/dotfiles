5c79ce40cbd2ff634c7f0fb20a24234c2de590a2
yukimura1227 <takamura1227@gmail.com>
2019-01-16 16:32:23 +0900

    chore: install vscode extentions tool

---
+++ for_mac_only/my_vscode_install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# NOTE: you can export extensions below
+# code --list-extensions > my_vscode_package.txt
+
+BASE_DIR=`dirname "${0}"`
+
+package_File_path="${BASE_DIR}/my_vscode_package.txt"
+
+for package_name in `cat ${package_File_path}`
+do
+  echo "code --install-extension ${package_name}"
+done
