7163c73cf72c1c9f24de6d0659d09fbc5969b00e
yukimura1227 <takamura1227@gmail.com>
2022-11-23 11:35:43 +0900

    feat: show bttery info and time on wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,5 +1,16 @@
 local wezterm = require 'wezterm'
 
+wezterm.on('update-right-status', function(window, pane)
+  local date = wezterm.strftime '🕰 %H:%M:%S '
+  local bat = ''
+  for _, b in ipairs(wezterm.battery_info()) do
+    bat = '🔋 ' .. string.format('%.0f%%', b.state_of_charge * 100)
+  end
+  window:set_right_status(wezterm.format {
+    { Text = bat .. '   ' .. date },
+  })
+end)
+
 return {
   font = wezterm.font_with_fallback {
     { family = "HackGen Console NFJ", weight = "Regular" },
