bb0271cbecd746db304075ed662a7071dc252529
yukimura1227 <takamura1227@gmail.com>
2023-01-02 22:52:53 +0900

    feat: add icon on tab

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -22,6 +22,40 @@ wezterm.on('format-window-title', function(tab, pane, tabs, panes, config)
   return index .. tab.active_pane.title .. '@' .. cwd_full
 end)
 
+local function basename(s)
+  return string.gsub(s, "(.*[/\\])(.*)", "%2")
+end
+local function detect_icon(foreground_process_name)
+  local nerd_icons_mapping = {
+    Vim    = wezterm.nerdfonts.custom_vim,
+    bash   = wezterm.nerdfonts.dev_terminal,
+    ssh    = wezterm.nerdfonts.mdi_server,
+    top    = wezterm.nerdfonts.mdi_monitor,
+    docker = wezterm.nerdfonts.dev_docker,
+    ruby   = wezterm.nerdfonts.dev_ruby,
+  }
+  local process_name = basename(foreground_process_name)
+  icon = nerd_icons_mapping[process_name]
+  if icon then
+    return icon .. ' '
+  else
+    return ''
+  end
+end
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+  local icon = detect_icon(tab.active_pane.foreground_process_name)
+  local tab_text = (tab.tab_index+1) .. ':  ' .. icon .. '  ' .. tab.active_pane.title
+  if tab.is_active then
+    local cwd = basename(tab.active_pane.current_working_dir)
+    return {
+      { Background = { Color = '#4000CF' } },
+      { Text = tab_text .. '  ' .. cwd .. '/' },
+    }
+  end
+  return tab_text
+end)
+
 return {
   audible_bell = 'Disabled',
   font = wezterm.font_with_fallback {
