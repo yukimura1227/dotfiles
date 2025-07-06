c7836e63ecb026d08dd7b54e6c37b800ae6fc4a4
yukimura1227 <takamura1227@gmail.com>
2025-07-06 10:24:25 +0900

    feat: update process -> icon mapping

--- .config/wezterm/tabs.lua
+++ .config/wezterm/tabs.lua
@@ -16,12 +16,29 @@ end)
 
 local function detectIcon(foregroundProcessName)
   local nerdIconsMapping = {
-    Vim    = wezterm.nerdfonts.custom_vim,
-    bash   = wezterm.nerdfonts.dev_terminal,
-    ssh    = wezterm.nerdfonts.mdi_server,
-    top    = wezterm.nerdfonts.mdi_monitor,
-    docker = wezterm.nerdfonts.dev_docker,
-    ruby   = wezterm.nerdfonts.dev_ruby,
+    -- editor
+    Vim     = wezterm.nerdfonts.dev_vim,
+    -- shell
+    bash    = wezterm.nerdfonts.dev_terminal,
+    zsh     = wezterm.nerdfonts.dev_terminal,
+    -- programming lanuage
+    ruby    = wezterm.nerdfonts.dev_ruby,
+    node    = wezterm.nerdfonts.md_nodejs,
+    -- git
+    tig     = wezterm.nerdfonts.md_git,
+    lazygit = wezterm.nerdfonts.md_git,
+
+    -- find tool
+    fzf     = wezterm.nerdfonts.md_feature_search_outline,
+    rg      = wezterm.nerdfonts.md_feature_search_outline,
+    grep    = wezterm.nerdfonts.md_feature_search_outline,
+    find    = wezterm.nerdfonts.md_feature_search_outline,
+    yazi    = wezterm.nerdfonts.md_file_tree_outline,
+
+    -- misc
+    ssh     = wezterm.nerdfonts.mdi_server,
+    top     = wezterm.nerdfonts.mdi_monitor,
+    docker  = wezterm.nerdfonts.dev_docker,
   }
   local processName = basename(foregroundProcessName)
   icon = nerdIconsMapping[processName]
